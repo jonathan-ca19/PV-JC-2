3c9538c18eddd83d181fb85645830da5e6a45edf
jonathan-ca19 <jap.canar@yavirac.edu.ec>
2023-10-26 19:15:09 -0500

    26-10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from './shared/shared.module';
+import { NoPagesFoundComponent } from './no-pages-found/no-pages-found.component';
+import { AuthModule } from './auth/auth.module';
+import { WebModule } from './web/web.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NoPagesFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    AuthModule,
+    WebModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
